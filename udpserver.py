0df9236fb7f96860d22e369adad19ff6f07ae772
Armaan <mdarmaan99@gmail.com>
2026-07-19 04:30:58 +0530

    files from windows

--- udpserver.py
+++ udpserver.py
@@ -4,6 +4,6 @@ serversocket = socket(AF_INET,SOCK_DGRAM)
 serversocket.bind(('', serverPort))
 print("The server is ready to receive")
 while True:
-    message,clientaddress = serversocket.recvfrom(2048)
+    message, addr = serversocket.recvfrom(2048)
     modifiedmessage = message.decode().count('a')  # Count occurrences of the letter 'a'
-    serversocket.sendto(str(modifiedmessage).encode(),clientaddress)
+    serversocket.sendto(str(modifiedmessage).encode(), addr)
