ddf2812a6a34f4e747a19fc3ae5fc9ed9fe62557
Armaan <mdarmaan99@gmail.com>
2026-07-19 21:37:19 +0530

    DUplicate files from main

--- udpserver.py
+++
@@ -1,9 +0,0 @@
-from socket import *
-serverPort = 12000
-serversocket = socket(AF_INET,SOCK_DGRAM)
-serversocket.bind(('', serverPort))
-print("The server is ready to receive")
-while True:
-    message,clientaddress = serversocket.recvfrom(2048)
-    modifiedmessage = message.decode().count('a')  # Count occurrences of the letter 'a'
-    serversocket.sendto(str(modifiedmessage).encode(),clientaddress)
